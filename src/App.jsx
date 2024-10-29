46a4c14f2a78e3970d734f62f68e6a946f33d80c
Sai Smruti Ranjan Das <160756794+saismrutiranjan18@users.noreply.github.com>
2024-10-29 21:24:14 +0530

    Joke Gen React

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Joker from './Joker'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     < Joker />
+    </>
+  )
+}
+
+export default App
